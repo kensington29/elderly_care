910bf637d2afa316ec6fad674d193d8f1ec1190c
H.Kubota <mini.kensington.29@gmail.com>
2020-08-03 01:15:16 +0900

    写真から顔を検出し、切り出し

--- face_detection.py
+++ face_detection.py
@@ -1,14 +1,58 @@
-import cv2
+import requests
+from PIL import Image
+from io import BytesIO
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import re
 import os
 from openvino.inference_engine import IENetwork, IEPlugin
-import plot_image
+
+def resize_frame(frame, height):
+
+    # resize image with keeping frame width
+    scale = height / frame.shape[1]
+    frame = cv2.resize(frame, dsize=None, fx=scale, fy=scale)
+
+    return frame
+
+def get_frame(image):
+    
+    resize_width = 640
+
+    if re.match(r'http.?://', image):
+        response = requests.get(image)
+        frame = np.array(Image.open(BytesIO(response.content)))
+    else:
+        frame = cv2.imread(image)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    if frame.shape[1] >= resize_width:
+        frame = resize_frame(frame, resize_width)
+    
+    return frame
+
+
+
+# plot setting
+rows = 6
+columns = 6
+plt.rcParams['figure.figsize'] = (15.0, 15.0)
+
 
 # Read Image
 image = "img_reged_person/hitoshi.jpg"
 
 # create image
-frame = plot_image.plot_image(image)
+#init_frame, frame = plot_image.plot_image(image)
+frame = get_frame(image)
+frame_h, frame_w = frame.shape[:2]
+init_frame = frame.copy()
+
+print("frame_h, frame_w:{}".format(frame.shape[:2]))
+plt.figure(figsize=(8, 8))
+plt.imshow(frame)
+plt.show()
  
 # ターゲットデバイスの指定 
 plugin = IEPlugin(device="MYRIAD")
