2a1d37709cbb54240ac006d900f4ab1c9cc04325
H.Kubota <mini.kensington.29@gmail.com>
2020-08-03 11:29:02 +0900

    顔の角度調整
    顔のランドマーク（目鼻などの位置）を検出し、顔の角度を調整

--- face_detection.py
+++ face_detection.py
@@ -33,6 +33,29 @@ def get_frame(image):
     return frame
 
 
+def align_face(face_frame, landmarks):
+   
+    left_eye, right_eye, tip_of_nose, left_lip, right_lip = landmarks
+    
+    # compute the angle between the eye centroids
+    dy = right_eye[1] - left_eye[1]     # right eye, left eye Y
+    dx = right_eye[0] - left_eye[0]  # right eye, left eye X
+    angle = np.arctan2(dy, dx) * 180 / np.pi
+    # compute center (x, y)-coordinates (i.e., the median point)
+    # between the two eyes in the input image
+    ##eyes_center = ((right_eye[0] + left_eye[0]) // 2, (right_eye[1] + left_eye[1]) // 2)
+    
+    ## center of face_frame
+    center = (face_frame.shape[0] // 2, face_frame.shape[1] // 2)
+    h, w, c = face_frame.shape
+    
+    # grab the rotation matrix for rotating and scaling the face
+    M = cv2.getRotationMatrix2D(center, angle, scale=1.0)
+    aligned_face = cv2.warpAffine(face_frame, M, (w, h))
+    
+    return aligned_face
+
+
 
 # plot setting
 rows = 6
@@ -42,6 +65,11 @@ plt.rcParams['figure.figsize'] = (15.0, 15.0)
 
 # Read Image
 image = "img_reged_person/hitoshi.jpg"
+#image = "img_reged_person/cherry.jpg"
+#image = "img_reged_person/mari_1.jpg"
+#image = "img_reged_person/mari_2.jpg"
+#image = "img_reged_person/banqet.jpg"
+
 
 # create image
 #init_frame, frame = plot_image.plot_image(image)
@@ -98,4 +126,77 @@ for face_id, face in enumerate(faces[0][0]):
     ax.set_title("face_id:{}".format(face_id))
     plt.imshow(face_frame)
     face_id += 1
+plt.show()
+
+# Get facial landmarks to align faces
+# 1.Read IR
+# model_xml = fp_path + "FP16/landmarks-regression-retail-0009.xml"
+#model_xml = fp_path + "facial-landmarks-35-adas-0001.xml"
+# model_bin = os.path.splitext(model_xml)[0] + ".bin"
+net = IENetwork(model="FP16/landmarks-regression-retail-0009.xml", weights="FP16/landmarks-regression-retail-0009.bin")
+
+
+# 2. Configure input & putput
+input_blob = next(iter(net.inputs))
+out_blob = next(iter(net.outputs))
+n, c, h, w = net.inputs[input_blob].shape
+print("input:{}\noutput:{}".format(net.inputs,net.outputs))
+print("input.shape:{}\noutput.shape:{}".format(
+    net.inputs[input_blob].shape, net.outputs[out_blob].shape))
+
+# 3. Load Model
+exec_net = plugin.load(network=net, num_requests=2)
+
+# 4. Create Async Request
+
+# create ndarray (face_count, 5, 2) : shape of landmarks matrix (5, 2) 
+
+facial_landmarks = np.zeros((faces.shape[2], 5, 2)) 
+
+for face_id, face_frame in enumerate(face_frames):
+    in_frame = cv2.resize(face_frame, (w, h))
+    in_frame = in_frame.transpose((2, 0, 1))
+    in_frame = in_frame.reshape((n, c, h, w))
+    exec_net.infer(inputs={input_blob: in_frame})
+
+    # 5. Get Response
+    res = exec_net.requests[0].outputs[out_blob].reshape(1, 10)[0]
+
+    # 6. draw Response
+    lm_face = face_frame.copy()
+    for i in range(res.size // 2):
+        normed_x = res[2 * i]
+        normed_y = res[2 * i + 1]
+        x_lm = lm_face.shape[1] * normed_x
+        y_lm = lm_face.shape[0] * normed_y
+        cv2.circle(lm_face, (int(x_lm), int(y_lm)), 1 + int(0.03 * lm_face.shape[1]), (255, 255, 0), -1)
+        # save landmarks
+        facial_landmarks[face_id][i] = (x_lm, y_lm)
+        ax = plt.subplot(rows, columns, face_id + 1)
+        ax.set_title("face_id:{}".format(face_id))
+    plt.imshow(lm_face)
+plt.show()
+
+
+
+# display aligned faces
+aligned_faces = []
+
+# display input faces
+plt.figure()
+for face_id, face_frame in enumerate(face_frames):
+    ax = plt.subplot(rows, columns, face_id + 1)
+    ax.set_title("before face:{}".format(face_id))
+    plt.imshow(face_frame)
+plt.show()
+
+# display output faces
+plt.figure()
+for face_id, face_frame in enumerate(face_frames):
+    aligned_face = face_frame.copy()
+    aligned_face = align_face(aligned_face, facial_landmarks[face_id])
+    aligned_faces.append(aligned_face)
+    ax = plt.subplot(rows, columns, face_id + 1)
+    ax.set_title("after face:{}".format(face_id))
+    plt.imshow(aligned_face)
 plt.show()
